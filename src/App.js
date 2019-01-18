fa2ad3b470c9821dd41803157ef742120b3cebb3
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-01-18 15:13:52 -0600

    Inital React app started Route Components active
    Dummy data imported

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import AvengersList from "./components/AvengersList";
+
+import "./App.css";
+import avengers from "./data";
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      avengers : avengers
+    };
+  }
+  
+  
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Route exact path="/" component= {Home}/>
+        <Route path="/avengers" component= {AvengersList} />
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home () {
+    return(
+        <div>
+            <h1> Avengers database </h1>
+            <h3> Welcome to the Super Secret Boyband! </h3>
+        </div>
+    );
+}
+
+export default Home;
