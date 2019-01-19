dceb5ff4f343b3cc2cc881126b6c060f6e7ab6d1
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-01-19 12:03:42 -0600

    Link Compoent set in all paths of the project

--- src/components/AvengersList.js
+++ src/components/AvengersList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 import avengers from "../data";
 
@@ -8,7 +9,9 @@ function AvengersList () {
             {avengers.map(avenger => (
                 <div className='character-card' key={avenger.id}>
                     <img src= {avenger.thumbnail} alt= {avenger.name} />
-                    <h2>{avenger.name}</h2>
+                    <h2>
+                    <Link to= {`/avengers/${avenger.id}`}>{avenger.name}</Link>
+                    </h2>
                     <p>({avenger.nickname})</p>
                 </div>
             ))}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Home () {
     return(
         <div>
             <h1> Avengers database </h1>
             <h3> Welcome to the Super Secret Boyband! </h3>
+            <Link to="/avengers">Enter</Link>
         </div>
     );
 }
