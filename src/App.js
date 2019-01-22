9ca2e2a127ca7c392e12629a1ae99fdccfd224d2
Megan Fontenot <lambda_school_loaner_28@Lambda-School-Loaner-28s-MacBook-Pro.local>
2019-01-22 17:35:39 -0600

    Render Props and Nested Props running

--- src/App.js
+++ src/App.js
@@ -12,13 +12,14 @@ class App extends Component {
   constructor () {
     super();
     this.state = {
-      avenger : avengers
+      avengers : avengers
     };
   }
   
   
   
   render() {
+    const { avengers } = this.state;
     return (
       <div className="App">
         <ul className="navbar">
@@ -30,9 +31,9 @@ class App extends Component {
           </li>
         </ul>
         <Route exact path="/" component= {Home}/>
-        <Route exact path="/avengers" component= {AvengersList} />
-        <Route path="/avengers/:id" component= {AvengerPage} />
-      </div>
+        <Route exact path="/avengers" render= {props => <AvengersList {...props} avengers={avengers}/>} />
+        <Route path="/avengers/:id" render={props => <AvengerPage {...props} avengers={avengers} /> } />
+        </div>
     );
   }
 }
--- src/components/AvengerPage.js
+++ src/components/AvengerPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
-import avengers from '../data';
+import AvengerDetails from "./AvengerDetails";
+import AvengerMovies from "./AvengerMovies";
 
 function AvengerPage (props) {
     const id = props.match.params.id;
-    const avenger = avengers.find(avenger => `${avenger.id}` === id);
+    const avenger = props.avengers.find(avenger => `${avenger.id}` === id);
     return(
         <div>
             <img
@@ -15,8 +17,11 @@ function AvengerPage (props) {
             <div className= "character-info-wrapper">
                 <h1>{avenger.name}</h1> 
                 <h4>({avenger.nickname})</h4>
-                <p>{avenger.description}</p>
             </div>
+            <Link to={`/avengers/${props.match.params.id}/details`}>Avenger Details </Link>
+            <Link to={`/avengers/${props.match.params.id}/movies`}>Movies</Link>
+            <Route path= "/avengers/:id/details" render={props => <AvengerDetails details={avenger.description} /> }/>
+            <Route path= "/avengers/:id/movies" render={props => <AvengerMovies movies={avenger.movies}/> }/>
         </div>
     );
 }
